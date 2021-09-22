de53c717d16c23286f1bef888b50b27c3680c031
Divon2 <91172013+Divon2@users.noreply.github.com>
2021-09-22 10:35:58 +0700

    Add files via upload

---
+++ Algo 01 Desvon Akbar.py
@@ -0,0 +1,12 @@
+
+
+print ( "@@@   @@@@    @@@    @         @       @       @     @" ) 
+print ( "@  @  @       @       @       @      @   @     @ @   @ ") 
+print (" @  @  @@@@     @       @     @       @   @     @  @  @" ) 
+print ( "@ @   @         @       @   @        @   @     @   @ @") 
+print ( "@     @@@@    @@@         @            @       @     @ ") 
+
+print  ('he told me, "go grab your laundry"')  
+print   ("i'm making a bread")  
+
+ 
